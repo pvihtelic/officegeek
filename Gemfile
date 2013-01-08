d9a6d28f529729bd6ed57845dbd59e73770eb2dc
pvihtelic <patrick.vihtelic@gmail.com>
2013-01-08 10:54:34 -0600

    setting local env variables

--- Gemfile
+++ Gemfile
@@ -25,3 +25,6 @@ gem "figaro", ">= 0.5.0"
 gem "better_errors", ">= 0.3.2", :group => :development
 gem "binding_of_caller", ">= 0.6.8", :group => :development
 gem "hub", ">= 1.10.2", :require => nil, :group => [:development]
+gem "hominid"
+gem "google_visualr", ">= 2.1.2"
+gem "jquery-datatables-rails", ">= 1.10.0"
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,13 +70,10 @@ Officegeek::Application.configure do
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "example.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+      :address   => "smtp.mandrillapp.com",
+      :port      => 587,
+      :user_name => ENV["MANDRILL_USERNAME"],
+      :password  => ENV["MANDRILL_PASSWORD"]
   }
 
 
