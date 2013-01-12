dc16561277eda8ecdc5e35f38d903a83bca39c73
pvihtelic <patrick.vihtelic@gmail.com>
2013-01-12 09:51:21 -0600

    filling out bio

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -23,7 +23,7 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_inactive_sign_up_path_for(resource)
-    '/thankyou.html'
+    "/"
   end
 
   def after_sign_up_path_for(resource)
